4fc6b851e46b161ca57bc98ad385e9a0254600a6
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-17 09:29:19 -0500

    Initial Commit

---
+++ SkyrimHobbyCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkyrimHobbyCards
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-12-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkyrimHobbyCards/SkyrimHobbyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyrimHobbyCardsApp.swift
+//  SkyrimHobbyCards
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-12-17.
+//
+
+import SwiftUI
+
+@main
+struct SkyrimHobbyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
