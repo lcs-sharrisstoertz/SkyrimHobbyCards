a178d1a6b102385e92a2e7258cdb2ae6b2d5af58
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-17 09:46:11 -0500

    Mostly finished organization

---
+++ SkyrimHobbyCards/Views/CardTabView.swift
@@ -0,0 +1,21 @@
+//
+//  CardTabView.swift
+//  SkyrimHobbyCards
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-12-17.
+//
+
+import SwiftUI
+
+//struct CardTabView: View {
+    
+//    var body: some View {
+//        TabView {
+//            ForEach(cards) { currentCard in CardView(card: currentCard)}
+//        }
+//    }
+//}
+
+//#Preview {
+//    CardTabView()
+//}
