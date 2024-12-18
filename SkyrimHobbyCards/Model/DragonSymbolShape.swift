a7fb52af48b02fc7435682a2d857ef14a2905a96
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-17 19:30:03 -0500

    Made pointed rectangle shape for race/species text

---
+++ SkyrimHobbyCards/Model/DragonSymbolShape.swift
@@ -0,0 +1,18 @@
+//
+//  DragonSymbol.swift
+//  SkyrimHobbyCards
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-12-17.
+//
+
+import SwiftUI
+
+struct DragonSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DragonSymbol()
+}
--- SkyrimHobbyCards/Views/CardView.swift
+++ SkyrimHobbyCards/Views/CardView.swift
@@ -14,25 +14,28 @@ struct CardView: View {
         // Background
         Color(.darkBlue)
             .overlay(alignment: .center) {
-                    LinearGradient (colors: [.darkGray, .gray], startPoint: .top, endPoint: .bottom)
-                        .frame(width: 350, height: 725)
+                LinearGradient (colors: [.darkGray, .gray], startPoint: .top, endPoint: .bottom)
+                    .frame(width: 350, height: 725)
                 // Image
-                    Image("Brynjolf")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 325, height: 500, alignment: .center)
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 100, trailing: 0))
+                Image("Brynjolf")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 310, height: 500, alignment: .center)
+                    .clipped()
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 100, trailing: 0))
                 // Name
-                    Rectangle()
-                        .fill(.beige)
-                        .frame(width: 300, height: 60)
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 650, trailing: 0))
+                Rectangle()
+                    .fill(.beige)
+                    .frame(width: 500, height: 60)
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 685, trailing: 0))
                 Text ("Brynjolf")
                     .font(.custom("Papyrus", size: 35.0, relativeTo: .body))
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 650, trailing: 0))
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 685, trailing: 0))
+                
+                // Race/species
+                
+                
             }
-        
-        //
     }
 }
 
